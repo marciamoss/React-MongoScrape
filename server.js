8ed2136a3550cc296c969d2ac8a84319c6cd665d
Autumn4eva <45056799+marciamoss@users.noreply.github.com>
2021-02-04 15:12:05 -0500

    Changed mongodb to atlasMongodb

--- server.js
+++ server.js
@@ -16,7 +16,17 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactnews");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactnews");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/reactnews",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Start the API server
 app.listen(PORT, function() {
